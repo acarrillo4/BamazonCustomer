294583f25016dccc435164880b22e207b0c97068
Carrillo <acarrillo@cars.com>
2018-02-20 00:01:44 -0600

    MYSQL DB files and bamazonCustomer.js file

---
+++ bamazonData.sql
@@ -0,0 +1,30 @@
+-- Data used to populate tables
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Jordan XI', 'Shoes', 210.00, 5);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Jordan XIII', 'Shoes', 180.00, 8);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Jordan 6 Rings', 'Shoes', 140.00, 6);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Jordan Hoodie', 'Apparel', 160.00, 10);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Jordan Track Pants', 'Apparel', 160.00, 6);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Jordan 3PK Jumpman Crew Socks', 'Accessories', 30.00, 10);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('NIKE AIR MAX', 'Shoes', 110.00, 7);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('NIKE Hoodie', 'Apparel', 120.00, 14);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('NIKE Track Pants', 'Apparel', 120.00, 10);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('NIKE 6PK Crew Socks', 'Accessories', 25.00, 12);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Adidas D Rose', 'Shoes', 130.00, 18);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Adidas Hoodie', 'Apparel', 100.00, 15);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Adidas Track Pants', 'Apparel', 120.00, 12);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Adidas 2PK Traxion Menace Crew Socks', 'Accessories', 20.00, 12);
+
