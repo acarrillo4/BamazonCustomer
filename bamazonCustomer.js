9051311e3f994626e91c4b1de6fad0941ba268a9
acarrillo4 <31912403+acarrillo4@users.noreply.github.com>
2018-03-08 19:21:34 -0600

    Update bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "MSNickers4",
+    password: "******",
     database: "bamazon"
 });
 
